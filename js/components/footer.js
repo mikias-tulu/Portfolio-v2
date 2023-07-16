6c089be28361277970c81efc776b92394d363ccd
mikias-tulu <miki.tulu@gmail.com>
2023-07-16 23:53:22 +0300

    feat: added footer section with basic description

---
+++ js/components/footer.js
@@ -0,0 +1,35 @@
+export default class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <footer>
+          <div class="container">
+            <ul class="horizontal-list footer-socials" style="font-size: 30px;">
+                <li><a href="https://www.linkedin.com/in/" rel="noreferrer"
+                target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="https://github.com/mikias-tulu" rel="noreferrer" target="_blank"><i
+                    class="fab fa-github"></i></a></li>
+                <li><a href="mailto:miki.tulu@gmail.com" class="fas fa-envelope"></a></li>
+            </ul>
+            <ul class="horizontal-list menu-footer">
+                <li><a href="#landing-section">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul><br>
+          </div>
+        </footer>
+        <div class="sub-footer">
+            <div class="container">
+            <p>Mikias Tulu | Portfolio 2023</p>
+            </div>
+        </div>
+      `;
+    }
+  }
+  
+  customElements.define('footer-component', Footer);
